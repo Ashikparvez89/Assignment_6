5a2f70e7c5d6a4e66ba9f1f74bced7c9e86ca4a4
Ashikparvez89 <ashikparvez89@gmail.com>
2024-03-03 03:31:11 +0800

    Basic design added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js}", "./js/**/*.{js}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
-
+};
